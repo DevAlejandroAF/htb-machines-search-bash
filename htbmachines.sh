9370604d347b87d93aa913d03f6d7c7e83c31452
DevAlejandroAF <alejandroavi95@hotmail.com>
2023-08-17 18:29:29 -0500

    Download and actualization for files, added

--- htbmachines.sh
+++ htbmachines.sh
@@ -13,12 +13,18 @@ endColour="\033[0m\e[0m"
 #Ctrl+c
 function ctrl_c(){
   echo -e "\n\n${redColour}[!]${endColour} Saliendo...\n"
-  exit 1 
+  tput cnorm && exit 1 
 }
 trap ctrl_c INT
 
+# Variables globales
+main_url="https://htbmachines.github.io/bundle.js"
+
 function helpPanel(){
-  echo -e "\n[+] Uso:"
+  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Uso:${endColour}"
+  echo -e "\t${purpleColour}u)${endColour}${grayColour} Descargar o actualizar archivos necesarios${endColour}"
+  echo -e "\t${purpleColour}m)${endColour}${grayColour} Buscar por un nombre de máquina${endColour}"
+  echo -e "\t${purpleColour}h)${endColour}${grayColour} Mostrar este panel de ayuda${endColour}\n"
 }
 
 function searchMachine(){
@@ -26,18 +32,51 @@ function searchMachine(){
   echo "$machineName"
 }
 
+function updateFiles(){
+  if [ ! -f bundle.js ]; then
+    tput civis
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Archivos necesarios no encontrados. Descargando...${endColour}\n"
+    curl -s $main_url > bundle.js
+    js-beautify bundle.js | sponge bundle.js
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Archivos descargados con exito. ${endColour}\n"
+    tput cnorm
+  else
+    tput civis
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Comprobando si existen actualizaciones...${endColour}\n"
+    curl -s $main_url > bundle_temp.js
+    js-beautify bundle_temp.js | sponge bundle_temp.js
+    md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
+    md5_original_value=$(md5sum bundle.js | awk '{print $1}')
+    if [ $md5_temp_value == $md5_original_value ]; then
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} No se detectaron actualizaciones. ${endColour}\n"
+      rm bundle_temp.js
+    else
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Se detectaron actualizaciones. ${endColour}\n"
+      sleep 1
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Actualizando...${endColour}\n"
+      rm bundle.js && mv bundle_temp.js bundle.js
+      echo -e "\n${yellowColour}[+]${endColour}${grayColour} Archivos actualizados con exito. ${endColour}\n"
+    fi
+    tput cnorm
+  fi
+  curl
+}
+
 # Indicadores
 declare -i parameter_counter=0
 
-while getopts "m:h" arg; do
+while getopts "m:uh" arg; do
   case $arg in
     m) machineName=$OPTARG; let parameter_counter+=1;;
+    u) let parameter_counter+=2;;
     h) ;;
   esac
 done
 
 if [ $parameter_counter -eq 1 ]; then
   searchMachine $machineName
+elif [ $parameter_counter -eq 2 ]; then
+  updateFiles
 else
   helpPanel
 fi
